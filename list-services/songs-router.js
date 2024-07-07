ecc6d9b0c6c8c2e9dc24d7184addd2626c44fee5
cdto17 <cdanielcdto@gmail.com>
2024-07-07 12:09:54 -0500

    Fiveth commit

--- list-services/songs-router.js
+++ list-services/songs-router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { readSongs } = require('./data/songs');  
 const router = express.Router();
 
-// Ruta para obtener todas las canciones
+
 router.get('/songs', (req, res) => {
     try {
         const songs = readSongs();
