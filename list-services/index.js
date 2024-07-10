e639146e39069941807ea055b189785119cd1885
cdto17 <cdanielcdto@gmail.com>
2024-07-10 01:07:53 -0500

    Update Worflow

--- list-services/index.js
+++ list-services/index.js
@@ -7,6 +7,7 @@ app.use(bodyParser.json());
 
 app.use('/api', songsRouter);
 
+
 app.listen(3002, () => {
     console.log('Songs Listing Service running on port 3002');
     console.log('http://localhost:8082/api/songs/');
